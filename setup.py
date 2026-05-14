c46c3409be4ed968ceecb24ec3d1d963b172df85
Balaji03760816 <balajibalubj376@gmail.com>
2026-05-14 13:51:27 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages,setup
+
+
+setup(
+    name="MLproject",
+    version="0.0.1",
+    author="Balaji",
+    author_email="balaji.it0816@gmail.com",
+    packages=find_packages(),
+    install_requires=["pandas","numpy","seaborn"]
+    
+)
